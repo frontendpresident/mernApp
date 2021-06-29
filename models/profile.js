753a028b7216e2999be4840242b27e4289431600
Sergey Maifetov <smaifetov.exceedteam@gmail.com>
2021-06-29 15:32:10 +0300

    created profile schema

---
+++ models/profile.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const profileSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    handle: {
+        type: String,
+        required: true,
+        max: 40
+    },
+    company: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    skills: {
+        type: [String],
+        required: true
+    },
+    bio: {
+        type: String
+    },
+    github: {
+        type: String
+    },
+    education: [
+        {
+            school: {
+                type: String,
+                required: true
+            },
+            degree: {
+                type: String,
+                required: true
+            },
+            fieldofstudy: {
+                type: String,
+                required: true
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date,
+                required: true
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            discriptions: {
+                type: String
+            }
+        }
+    ],
+    social: {
+        facebook: {
+            type: String
+        },
+        instagram: {
+            type: String
+        }
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Profile = mongoose.model('profile', profileSchema);
