3849f2a73ce19c37f3a187e9b1c2f3d8dd4d2263
Sergey Maifetov <smaifetov.exceedteam@gmail.com>
2021-07-02 16:41:38 +0300

    added Dashboard component and preloader

---
+++ client/src/components/common/Preloader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import preloader from '../../img/preloader.png'
+
+function Preloader() {
+    return (
+        <div class="text-center">
+            <img src={preloader} classNa="rounded ml-2" alt="..." />
+        </div>
+    )
+}
+
+export default Preloader;
+
+
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import propTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getCurrentProfile } from '../../redux/reducers/profileReducer'
+import Preloader from '../common/Preloader'
 
 class Dashboard extends React.Component {
     constructor(props) {
@@ -12,17 +13,49 @@ class Dashboard extends React.Component {
     }
 
     render() {
+        const { user } = this.props.auth
+        const { profile, loading } = this.props.profile
+
+        let dashboardContent;
+
+        if (profile === null || loading) {
+            dashboardContent = <Preloader />
+        }
+        else {
+            dashboardContent = (
+                <h1>test</h1>
+            )
+        }
         return (
             <div>
-
+                <div class="dashboard">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h1 class="display-4">Dashboard</h1>
+                                <div class="btn-group mb-4" role="group">
+                                    <h1>TEST</h1>                                 
+                                </div>
+                                {dashboardContent}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
+Dashboard.propTypes = {
+    getCurrentProfile: propTypes.func.isRequired,
+    auth: propTypes.object.isRequired,
+    profile: propTypes.object.isRequired
+}
+
 const mapStateToProps = (state) => {
     return {
-        profile: state.profile
+        profile: state.profile,
+        auth: state.auth
     }
 }
 export default connect(mapStateToProps, { getCurrentProfile })(Dashboard)
