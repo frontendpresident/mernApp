939140c8840a0f72dbffebbd22bc859cbf1ae50c
Sergey Maifetov <smaifetov.exceedteam@gmail.com>
2021-07-06 10:03:34 +0300

    heroku deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors')
 const mongoose = require('mongoose');
 const passport = require('passport')
+const path = require('path')
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -22,14 +23,14 @@ app.get('/', (req, res) => {
 const db = require('./config/keys').URL
 
 //Body parser
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 
 mongoose
-   .connect(db, { 
+   .connect(db, {
       useUnifiedTopology: true,
       useNewUrlParser: true
-    })
+   })
    .then(() => console.log('Connect date base MongoDB'))
    .catch((err) => console.log(`Error: ${err}`))
 
@@ -39,6 +40,14 @@ require('./config/passport')(passport)
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static('client/build'))
+   
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+   })
+}
 
 const port = process.env.PORT || 5000;
 
